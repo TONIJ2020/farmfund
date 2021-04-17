42031e9ce29f3f392552494f9ba259547ad10b97
tonij2020 <69843584+naccyworld@users.noreply.github.com>
2021-04-17 23:18:59 +0100

    Mobile-responsiveness

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -14,57 +14,67 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div className="footer-container">
-            <div className="footer-main-div">
-                <div className="footer-prim-div">
-                    <p className="footer-prim-paragraph-text">Invest or get Investment and full time support to grow and sell your crops</p>
+        <div className="footer-main-div">
+            <div className="footer-prim-div">
+                <p className="footer-prim-paragraph-text">Invest or get Investment and full time support to grow and sell your crops</p>
 
-                    <Link to="/register"><button className="footer-prim-button">GET STARTED</button></Link>
+                <Link to="/register"><button className="footer-prim-button">GET STARTED</button></Link>
+            </div>
 
-                    <div className="footer-sec-div">
-                        <img src={Smooth} alt="" className="footer-smooth-img" />
+            <div className="footer-sec-div">
+                <img src={Smooth} alt="" className="footer-smooth-img" />
 
-                        <a className="footer-support-acrotag" href="#support"><img src={Support} alt="" className="footer-support-img" />24hr Support</a>
+                <a className="footer-support-acrotag" href="#support"><img src={Support} alt="" className="footer-support-img" />24hr Support</a>
 
-                        <img src={Payment} alt="" className="footer-payment-img" />
+                <img src={Payment} alt="" className="footer-payment-img" />
 
-                        <a className="footer-farmfund-acrotag" href="#farmfund"><img src={FarmFund} alt="" className="footer-farmfund-img" />FarmFund</a>
-                    
-                        <hr className="footer-line"/>
+                <a className="footer-farmfund-acrotag" href="#farmfund"><img src={FarmFund} alt="" className="footer-farmfund-img" />FarmFund</a>
+            </div>
 
-                        <h3 className="who-we-are-text">Who We Are</h3>
+            <hr className="footer-line"/>
+            
+            <div className="footer-ter-div">
+                <div className="who-we-are-div">
+                    <h3 className="who-we-are-text">Who We Are</h3>
 
-                        <h3 className="quick-link-text">Quick Links</h3>
-
-                        <h3 className="contact-us-text">Contact Us</h3>
-
-                        <ul className="who-we-are-list">
-                            <li>About</li>
-                            <li>Team</li>
-                            <li>Work With Us</li>
-                        </ul>
-
-                        <ul className="quick-link-list">
-                        <Link to="/login"><li className="list-1">Login</li></Link>
-                            <Link to="/register"><li className="list-2">Register</li></Link>
-                            <Link to="/farmer"><li className="list-3">Projects</li></Link>
-                        </ul>
-
-                        <ul className="contact-us-list">
-                            <li>+234 706 849 7582</li>
-                            <li>+234 803 886 8586</li>
-                            <li>contact@farmfund.com</li>
-                        </ul>
-                    </div>
+                    <ul className="who-we-are-list">
+                        <li>About</li>
+                        <li>Team</li>
+                        <li>Work With Us</li>
+                    </ul>
                 </div>
 
-                <img src={Twitter} alt="" className="footer-twitter-img" />
+                <div className="quick-link-div">
+                    <h3 className="quick-link-text">Quick Links</h3>
 
-                <img src={Facebook} alt="" className="footer-facebook-img" />
+                    <ul className="quick-link-list">
+                    <Link to="/login"><li className="list-1">Login</li></Link>
+                        <Link to="/register"><li className="list-2">Register</li></Link>
+                        <Link to="/farmer"><li className="list-3">Projects</li></Link>
+                    </ul>
+                </div>
 
-                <img src={Feed} alt="" className="footer-feed-img" />
+                <div className="contact-div">
+                    <h3 className="contact-us-text">Contact Us</h3>
 
-                <img src={Instagram} alt="" className="footer-instagram-img" />
+                    <ul className="contact-us-list">
+                        <li>+234 706 849 7582</li>
+                        <li>+234 803 886 8586</li>
+                        <li>contact@farmfund.com</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer-bottom-div">
+                <div className="footer-social">
+                    <img src={Twitter} alt="" className="footer-twitter-img" />
+
+                    <img src={Facebook} alt="" className="footer-facebook-img" />
+
+                    <img src={Feed} alt="" className="footer-feed-img" />
+
+                    <img src={Instagram} alt="" className="footer-instagram-img" />
+                </div>
 
                 <p className="copyright-text">COPYRIGHT @ FARM FUND 2021</p>
             </div>
