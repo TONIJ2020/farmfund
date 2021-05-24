f4aa65834afd27528b281022d4d795fee0c2e033
tonij2020 <69843584+naccyworld@users.noreply.github.com>
2021-05-24 15:03:21 +0100

    redirect to login on logout

--- src/Components/Pages/dashboard.js
+++ src/Components/Pages/dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+// import Login from '../LoginRegister/Login';
 import '../Styles/dashboard.css';
 
 export default class dashboard extends Component {
@@ -11,45 +13,51 @@ constructor(props) {
 }
 
     render() {
-        return (
-            <div className='dashboard-div'>
-                <div className="welcome-message-div"><h1 className="welcome-message">Hello!  {this.state.firstName} {this.state.lastName}, Welcome to FarmFund.</h1>
-                <h4>We are happy to have you in our team and we look forward to working with you. Fill free to contact us at any time.</h4>
+        if(localStorage.getItem('userData')){
+            return (
+                <div className='dashboard-div'>
+                    <div className="welcome-message-div"><h1 className="welcome-message">Hello!  {this.state.firstName} {this.state.lastName}, Welcome to FarmFund.</h1>
+                    <h4>We are happy to have you in our team and we look forward to working with you. Fill free to contact us at any time.</h4>
+                    </div>
+
+
+
+                    <div className="personal-info-div"><h2 className="personal-info">PERSONAL INFORMATION</h2></div>
+
+                    <div>
+                        <h2 className="info">FULL NAME</h2>
+                        <span className="names">
+                            <p>First Name: {this.state.firstName}</p>
+                            <p>Middle Name: {this.state.middleName}</p>
+                            <p>
+                            Last Name: {this.state.lastName}</p>
+                        </span>
+                    </div>
+                    <hr />
+
+                    <div>
+                        <h2 className="info">CONTACT</h2>
+                        <span className="names">
+                            <p>Contact Address: {this.state.address}</p>
+                            <p>E-mail Address: {this.state.email}</p>
+                            <p>
+                            Contact Phone Number: {this.state.phone}</p>
+                        </span>
+                    </div>
+                    <hr />
+
+                    <div>
+                        <h2 className="info">DESIGNATION</h2>
+                        <span className="names">
+                            <p>Farmfund Designation: {this.state.track}</p>
+                        </span>
+                    </div>
                 </div>
-
-
-
-                <div className="personal-info-div"><h2 className="personal-info">PERSONAL INFORMATION</h2></div>
-
-                <div>
-                    <h2 className="info">FULL NAME</h2>
-                    <span className="names">
-                        <p>First Name: {this.state.firstName}</p>
-                        <p>Middle Name: {this.state.middleName}</p>
-                        <p>
-                        Last Name: {this.state.lastName}</p>
-                    </span>
-                </div>
-                <hr />
-
-                <div>
-                    <h2 className="info">CONTACT</h2>
-                    <span className="names">
-                        <p>Contact Address: {this.state.address}</p>
-                        <p>E-mail Address: {this.state.email}</p>
-                        <p>
-                        Contact Phone Number: {this.state.phone}</p>
-                    </span>
-                </div>
-                <hr />
-
-                <div>
-                    <h2 className="info">DESIGNATION</h2>
-                    <span className="names">
-                        <p>Farmfund Designation: {this.state.track}</p>
-                    </span>
-                </div>
-            </div>
-        )
+            );
+        }else{
+            return (
+            Redirect("/login")
+            );
+        }
     }
 }
